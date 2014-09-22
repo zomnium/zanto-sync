b40a61d90ed5c201ad3bd77ad447edb80397f828
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2014-09-22 18:40:35 +0200

    Changed coding style, added API outline and actions hooks

--- inc/sync-posts.php
+++ inc/sync-posts.php
@@ -4,21 +4,44 @@
  * Sync Posts for Zanto
  */
 
-class ZantoSyncPosts extends ZantoSyncComponent {
-
-	public function __construct() {
+class ZantoSyncPosts extends ZantoSyncComponent
+{
+	public function __construct()
+	{
+		// Load ZantoSyncComponent
 		parent::__construct();
+
+		// Set hooks
+		return $this->set_hooks();
 	}
 
-	public function set_hooks() {
-		add_action( 'the_post', array( $this, 'get_event' ) );
+	/**
+	 * Set hooks
+	 */
+
+	public function set_hooks()
+	{
+		add_action( 'wp_insert_post', array( $this, 'new_post' ) );
+		add_action( 'post_updated', array( $this, 'update_post' ), 10, 3 );
+		add_action( 'trashed_post ', array( $this, 'trash_post' ) );
+		add_action( 'untrashed_post  ', array( $this, 'untrash_post' ) );
+		add_action( 'before_delete_post  ', array( $this, 'delete_post' ) );
 	}
 
-	public function batch_process() {
+	public function batch_process()
+	{
 		//
 	}
 
-	public function sync_post() {
-		//
-	}
+	public function new_post() {}
+
+	public function update_post( $post_id, $post_after, $post_before ) {}
+
+	public function trash_post() {}
+
+	public function untrash_post() {}
+
+	public function delete_post() {}
+
+	public function sync_post() {}
 }
