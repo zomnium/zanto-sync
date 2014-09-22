11337639ab91d1ac4772253f19a9be645f89cd2e
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2014-09-22 22:45:54 +0200

    Coding style, comments and API outline

--- inc/sync-users.php
+++ inc/sync-users.php
@@ -4,22 +4,54 @@
  * Sync Users for Zanto
  */
 
-class ZantoSyncUsers extends ZantoSyncComponent {
-
-	public function __construct() {
+class ZantoSyncUsers extends ZantoSyncComponent
+{
+	public function __construct()
+	{
+		// Load ZantoSyncComponent
 		parent::__construct();
 	}
 
-	public function set_hooks() {
-		add_action( 'user_register', array( $this, 'sync_user' ) );
+	/**
+	 * Set hooks
+	 */
+
+	public function set_hooks()
+	{
+		add_action( 'user_register', array( $this, 'sync_user', 10, 1 ) );
 		add_action( 'profile_update', array( $this, 'sync_user' ) );
 	}
 
-	public function batch_process() {
-		//
-	}
+	/**
+	 * Batch process
+	 */
 
-	public function sync_user() {
+	public function batch_process() {}
+
+	/**
+	 * New user
+	 */
+
+	public function new_user( $user_id ) {}
+
+	/**
+	 * Update user
+	 */
+
+	public function update_user() {}
+
+	/**
+	 * Delete user
+	 */
+
+	public function delete_user() {}
+
+	/**
+	 * Sync user
+	 */
+
+	public function sync_user()
+	{
 		return add_user_to_blog( $blog_id, $user_id, $role );
 	}
 }
