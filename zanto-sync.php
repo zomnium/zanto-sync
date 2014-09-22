cb69673cbbeec48f33da0506395b555c340065e5
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2014-09-22 18:20:04 +0200

    Added some basic comments

--- zanto-sync.php
+++ zanto-sync.php
@@ -16,27 +16,58 @@ Author URI: http://zomnium.com/
 
 class ZantoSync {
 
+	// Singleton instance
 	private static $instance;
+
+	// Zanto settings
 	private $settings;
+
+	// Zanto Sync Components
 	private $components;
 
+	/**
+	 * Constructor
+	 * Initalizes the plugin
+	 */
 	public function __construct() {
 		self::$instance = $this;
+		$this->settings = get_option( 'zwt_zanto_settings', null );
 	}
 
+	/**
+	 * Get instance
+	 * Returns the singleton instance
+	 */
 	public static function get_instance() {
 		return self::$instance;
 	}
 
+	/**
+	 * Validate requirements
+	 * Executes a checklist for all plugin requirements
+	 */
 	public function validate_requirements() {}
 
+	/**
+	 * Found
+	 */
 	public function zwt_found() {}
 
+	/**
+	 * Network
+	 */
 	public function zwt_network() {}
 
+	/**
+	 * Primary
+	 */
 	public function zwt_primary() {}
 
+	/**
+	 * Current
+	 */
 	public function zwt_current() {}
 }
 
+// Let's run this basterd :)
 new ZantoSync;
