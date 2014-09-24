9b06d6ae3b3a2d0b5ec44c18bf2e79a5110a767c
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2014-09-24 22:05:34 +0200

    Added requirements validation and notes

--- inc/sync-connections.php
+++ inc/sync-connections.php
@@ -10,20 +10,57 @@ class ZantoSyncConnections extends ZantoSyncComponent
 	{
 		// Load ZantoSyncComponent
 		parent::__construct();
+
+		// Check requirements
+		if ( $this->validate_requirements() ) return false;
+
+		// Set hooks
+		$this->set_hooks();
 	}
+
+	/**
+	 * Validate requirements
+	 * Checks if everything is alright
+	 * @return boolean
+	 */
+
+	public function validate_requirements()
+	{
+		// Check for Posts 2 Posts
+		if ( ! defined( 'P2P_PLUGIN_VERSION' ) ) return false;
+
+		// Everything is goood
+		return true;
+	}
+
+	/**
+	 * Set hooks
+	 * Straps all hooks to actions and filters
+	 * @return null
+	 */
 
 	public function set_hooks()
 	{
 		add_action( 'the_post', array( $this, 'get_event' ) );
 	}
 
+	/**
+	 * Batch process
+	 * @todo implement this function
+	 */
+
 	public function batch_process()
 	{
-		//
+		// TODO: implement
 	}
+
+	/**
+	 * Sync connections
+	 * @return null
+	 */
 
 	public function sync_connections()
 	{
-		//
+		// TODO: implement
 	}
 }
