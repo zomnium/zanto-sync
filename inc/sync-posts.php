94800e920e507b9b9ed2d6a294896fb90e9675a1
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2014-09-24 22:06:35 +0200

    Changed hooks, started with updated_post() and sync_post()

--- inc/sync-posts.php
+++ inc/sync-posts.php
@@ -22,8 +22,9 @@ class ZantoSyncPosts extends ZantoSyncComponent
 
 	public function set_hooks()
 	{
-		add_action( 'wp_insert_post', array( $this, 'new_post' ) );
-		add_action( 'post_updated', array( $this, 'update_post' ), 10, 3 );
+		// add_action( 'wp_insert_post', array( $this, 'new_post' ) );
+		// add_action( 'post_updated', array( $this, 'update_post' ), 10, 3 );
+		add_action( 'save_post', array( $this, 'updated_post' ) );
 		add_action( 'trashed_post ', array( $this, 'trash_post' ) );
 		add_action( 'untrashed_post  ', array( $this, 'untrash_post' ) );
 		add_action( 'before_delete_post  ', array( $this, 'delete_post' ) );
@@ -40,16 +41,25 @@ class ZantoSyncPosts extends ZantoSyncComponent
 	}
 
 	/**
-	 * New post
-	 * @todo implement this function
+	 * Updated post
+	 * For new and updated posts
+	 * @param int $post_id
+	 * @return unknown
 	 */
 
-	public function new_post()
+	public function updated_post( $post_id )
 	{
-		switch_to_blog( $site_id );
-		// do something
-		restore_current_blog();
+		// Get post
+		$post = get_post( $post_id, ARRAY_A );
+
+		// Remove id
+		unset( $post['ID'] );
+
+		// Sync post
+		$this->sync_post()
 	}
+
+	public function new_post() {}
 
 	public function update_post( $post_id, $post_after, $post_before ) {}
 
@@ -59,5 +69,35 @@ class ZantoSyncPosts extends ZantoSyncComponent
 
 	public function delete_post() {}
 
-	public function sync_post() {}
+	/**
+	 * Sync post
+	 * Handles post network syncing
+	 * @param array $post
+	 * @return null
+	 */
+
+	public function sync_post( $post )
+	{
+		// Get network blogs
+		$network = $this->zanto_sync->get_network_blogs();
+
+		// Get current language
+		$current_language = $this->zanto_sync->get_current_language();
+
+		// Loop through network blogs
+		foreach ($network as $blogs)
+		{
+			// Exclude current blog from syncing
+			if ( $blog['lang_code'] != $current_language )
+			{
+				// Sync post
+				switch_to_blog( $blog['blog_id'] );
+				wp_insert_post( $post );
+				// TODO: add translation link
+			}
+		}
+
+		// Restore current blog
+		restore_current_blog();
+	}
 }
